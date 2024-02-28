d3e9b9aab3c5c56add34bbf50111f2dad1a65ecd
Vshal007 <vshalpatel018@gmail.com>
2024-02-28 19:41:36 +0530

    footer links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa"
+import { FaGlobe } from "react-icons/fa"
+import { IoCall } from "react-icons/io5"
+
 
 const Footer = () => {
     return (
-        <footer className='p-10 bg-red-400 m-1'>
-          
+        <footer className='p-5 pb-10 bg-red-400 m-1 flex justify-between text-white font-serif flex-col md:flex-row'>
+            <div className='flex space-x-2'><IoCall /> Toll free 1800 1200 1234</div>
+            <div className='flex space-x-2'><FaFacebook /> <p><a href="https://www.facebook.com/cripumps">www.facebook.com/cripumps</a></p></div>
+            <div className='flex space-x-2'><FaGlobe /> <p><a href="https://www.crigroups.com">www.crigroups.com</a></p></div>
         </footer>
     )
 }
